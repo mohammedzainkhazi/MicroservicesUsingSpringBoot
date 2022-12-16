05fb94294d0135996229625c16c2ac1c24492341
mohammedzainkhazi <zainhamza@outlook.com>
2022-12-17 00:45:51 +0530

    Created communication point between two services

--- lab-service/src/main/java/com/example/mslabspringboottest/controller/LabController.java
+++ lab-service/src/main/java/com/example/mslabspringboottest/controller/LabController.java
@@ -3,12 +3,24 @@ package com.example.mslabspringboottest.controller;
 import com.example.mslabspringboottest.model.LabSystem;
 import com.example.mslabspringboottest.service.LabService;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.http.*;
+import java.util.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api")
 public class LabController {
+    @Autowired
+    private RestTemplate restTemplate;
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
     private final LabService service;
 
     public LabController(LabService service) {
@@ -38,5 +50,12 @@ public class LabController {
     public String deleteSystem(@PathVariable String id){
         service.deleteSystem(id);
         return "Deleted System : "+id;
+    }
+    @GetMapping("/all")
+        public List<LabSystem> getAllSystems(){
+        HttpHeaders headers=new HttpHeaders();
+        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+        HttpEntity<List> entity=new HttpEntity<List>(headers);
+        return restTemplate.exchange("http://localhost:3001/get/all",HttpMethod.GET,entity,List.class).getBody();
     }
 }
