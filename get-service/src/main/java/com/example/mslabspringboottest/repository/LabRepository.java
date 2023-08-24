506f24e09a4a0d7624bf6b9739d1ca1cce4dd0ce
RootZain <43783816+mohammedzainkhazi@users.noreply.github.com>
2023-08-24 10:32:32 +0530

    Formatted Code for learners.

--- get-service/src/main/java/com/example/mslabspringboottest/repository/LabRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.mslabspringboottest.repository;
-
-import com.example.mslabspringboottest.model.LabSystem;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface LabRepository extends JpaRepository<LabSystem,String> {
-}
