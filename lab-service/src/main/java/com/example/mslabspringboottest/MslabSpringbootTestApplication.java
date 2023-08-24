2f914a8b9d4989a77b0cf0b033e7770cb2e1ad5c
aditicodes-14 <108220656+aditicodes-14@users.noreply.github.com>
2023-08-24 11:36:07 +0530

    Added line 1

--- lab-service/src/main/java/com/example/mslabspringboottest/MslabSpringbootTestApplication.java
+++ lab-service/src/main/java/com/example/mslabspringboottest/MslabSpringbootTestApplication.java
@@ -10,6 +10,8 @@ public class MslabSpringbootTestApplication {
 
 	public static void main(String[] args) {
 		// SpringApplication.run(MslabSpringbootTestApplication.class, args);
+		System.out.println('Hello Zain!');
+
 		System.out.println('Hello Aditi!');
 	}
 
