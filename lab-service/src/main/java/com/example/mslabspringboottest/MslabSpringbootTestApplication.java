9310086ddeda4a94e63331ae82901da65e156769
Gurucharan046 <62395707+Gurucharan046@users.noreply.github.com>
2023-08-24 12:13:19 +0530

    added a pattern function

--- lab-service/src/main/java/com/example/mslabspringboottest/MslabSpringbootTestApplication.java
+++ lab-service/src/main/java/com/example/mslabspringboottest/MslabSpringbootTestApplication.java
@@ -7,10 +7,21 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @SpringBootApplication
 @EnableEurekaClient
 public class MslabSpringbootTestApplication {
-
+    public void pattern(int number_of_rows)
+	{ int iterator_inner,iterator_outer;
+		  for(iterator_outer=0;iterator_outer<number_of_rows;iterator_outer++)
+		  {
+			for (iterator_inner=0;iterator_inner<=iterator_outer;iterator_inner++)
+			{
+				   System.out.print('* ');
+			}
+			System.out.println();
+		  }
+	}
 	public static void main(String[] args) {
 		// SpringApplication.run(MslabSpringbootTestApplication.class, args);
 		System.out.println('Hello Gurucharan d k !');
+		pattern(5);
 	}
 
 }
