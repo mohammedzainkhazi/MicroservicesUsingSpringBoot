d667b3e2d0acd61acad21db6d8901ec0a267d6d9
Aditya Raj Singh <51378111+rsaditya@users.noreply.github.com>
2023-08-24 11:29:26 +0530

    Added Hustlers Message

--- lab-service/src/main/java/com/example/mslabspringboottest/MslabSpringbootTestApplication.java
+++ lab-service/src/main/java/com/example/mslabspringboottest/MslabSpringbootTestApplication.java
@@ -11,6 +11,7 @@ public class MslabSpringbootTestApplication {
 	public static void main(String[] args) {
 		// SpringApplication.run(MslabSpringbootTestApplication.class, args);
 		System.out.println('Hello Zain!');
+		System.out.println('Hello Hustlers!');
 	}
 
 }
