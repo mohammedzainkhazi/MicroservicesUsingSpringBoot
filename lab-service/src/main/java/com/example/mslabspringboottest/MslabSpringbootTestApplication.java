9bb653f6c5c5cc2a5b9724f1dfb61f03f13a0c69
aryanshah287 <99818273+aryanshah287@users.noreply.github.com>
2023-08-24 11:27:45 +0530

    added new text

--- lab-service/src/main/java/com/example/mslabspringboottest/MslabSpringbootTestApplication.java
+++ lab-service/src/main/java/com/example/mslabspringboottest/MslabSpringbootTestApplication.java
@@ -11,6 +11,8 @@ public class MslabSpringbootTestApplication {
 	public static void main(String[] args) {
 		// SpringApplication.run(MslabSpringbootTestApplication.class, args);
 		System.out.println('Hello Zain!');
+		System.out.println('Hello Aryan!');
+
 	}
 
 }
