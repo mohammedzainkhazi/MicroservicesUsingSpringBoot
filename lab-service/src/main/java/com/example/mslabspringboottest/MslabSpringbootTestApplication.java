4979115ce219d7e7ad8d5d739663cbaa0c2803fe
Deepansha Adlakha <78579520+deepansha14@users.noreply.github.com>
2023-08-24 11:30:04 +0530

    Added Deepansha

--- lab-service/src/main/java/com/example/mslabspringboottest/MslabSpringbootTestApplication.java
+++ lab-service/src/main/java/com/example/mslabspringboottest/MslabSpringbootTestApplication.java
@@ -11,7 +11,7 @@ public class MslabSpringbootTestApplication {
 	public static void main(String[] args) {
 		// SpringApplication.run(MslabSpringbootTestApplication.class, args);
 		System.out.println('Hello Deepansha!');
-		System.out.println('----');
+		System.out.println('-----');
 	}
 
 }
